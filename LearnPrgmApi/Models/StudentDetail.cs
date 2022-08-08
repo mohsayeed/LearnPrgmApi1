f1e92ac8f0c7a160e37d1ae0681e4dd1241ed2b4
mohsayeed <cs19btech11004@iith.ac.in>
2022-08-08 20:05:26 +0530

    Add project files.

---
+++ LearnPrgmApi/Models/StudentDetail.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearnPrgmApi.Models
+{
+    public partial class StudentDetail
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int? Class { get; set; }
+    }
+}
---
+++ LearnPrgmApi/Models/learnprgmContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace LearnPrgmApi.Models
+{
+    public partial class learnprgmContext : DbContext
+    {
+        public learnprgmContext()
+        {
+        }
+
+        public learnprgmContext(DbContextOptions<learnprgmContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<StudentDetail> StudentDetails { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentDetail>(entity =>
+            {
+                entity.ToTable("studentDetails");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Class).HasColumnName("class");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .HasColumnName("name");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
